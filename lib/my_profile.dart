78db00d2d166afbe1902d6e875d025d9e6cfe27f
abichengs01 <abichengs01@gmail.com>
2024-08-19 17:59:59 +0530

    My profile page fully completed with detailing

--- lib/my_profile.dart
+++ lib/my_profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import 'colors.dart';
 
@@ -11,7 +12,102 @@ class MyProfile extends StatefulWidget {
 
 class _MyProfileState extends State<MyProfile> {
   int _value=-1;
+  TextEditingController dateInput = TextEditingController();
+
+  void _openSelector()  {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+            height: 248,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child: Column(
+                  children: [
+                    Text("Choose",style: TextStyle(fontWeight: FontWeight.w500,fontSize: 18),),
+                    SizedBox(height: 20,),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 25.0,top: 5,),
+                      child: Row(
+                        children: [
+
+                          Container(width: 65,
+                            child: Column(
+                              children: [
+                                Image.asset("images/camera.png",width: 44,height: 40,),
+                                SizedBox(height: 2,),
+                                Text("Camera",style: TextStyle(fontSize: 12,fontWeight: FontWeight.w400),)
+                              ],
+                            ),
+                          ),
+                          SizedBox(width: 15,),
+                          Container(width: 65,
+                            child: Column(
+                              children: [
+                                Image.asset("images/gallery.png",width: 40,height: 31,),
+                                SizedBox(height: 5,),
+                                Text("Gallery",style: TextStyle(fontSize: 12,fontWeight: FontWeight.w400),)
+                              ],
+                            ),
+                          ),
+                          SizedBox(width: 15,),
+                          Container(width: 80,
+                            child: Column(
+                              children: [
+                                Image.asset("images/filemanager.png",height: 29,width: 40,),
+                                SizedBox(height: 5,),
+                                Text("File Manager",style: TextStyle(fontSize: 12,fontWeight: FontWeight.w400),)
+                              ],
+                            ),
+                          ),
+                          SizedBox(width: 10,),
+                          Container(width: 65,
+                            child: Column(
+                              children: [
+                                Image.asset("images/drive.png",height: 32,width: 36,),
+                                SizedBox(height: 7,),
+                                Text("Drive",style: TextStyle(fontSize: 12,fontWeight: FontWeight.w400),)
+                              ],
+                            ),
+                          ),
+
+
+                        ],
+                      )
+                    ),
+
+                    SizedBox(height: 40,),
+                    Container(height: 41,width: 314,
+                        child: ElevatedButton(onPressed:null,
+                          style: ButtonStyle(
+
+                              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                ),)),
+                          child: Text("Cancel",style: TextStyle(
+                            fontSize: 16,fontWeight: FontWeight.w600,
+                          ),),
+                        ))
+                  ],
+                ),
+
+
+              ),
+            )
+        );
+      },
+    );
+  }
+
   @override
+  void initState() {
+      dateInput.text = ""; //set the initial value of text field
+    // TODO: implement initState
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
@@ -27,13 +123,19 @@ class _MyProfileState extends State<MyProfile> {
         children: [
           SizedBox(height: 10,),
 
-          Center(
-            child: Stack(
-              children: [
-                Image.asset("images/hs/profile.png",height: 91,width: 91,),
-                Positioned(right: 5,bottom: 15,
-                    child: Icon(Icons.add_circle,size: 22,color: AppColor.primary,))
-              ],),),
+          InkWell(
+            onTap: (){
+              print('share');
+              _openSelector();
+            },
+            child: Center(
+              child: Stack(
+                children: [
+                  Image.asset("images/hs/profile.png",height: 91,width: 91,),
+                  Positioned(right: 5,bottom: 15,
+                      child: Icon(Icons.add_circle,size: 22,color: AppColor.primary,))
+                ],),),
+          ),
 
           Padding(
             padding: const EdgeInsets.all(20.0),
@@ -103,13 +205,38 @@ class _MyProfileState extends State<MyProfile> {
                 Container(height: 40,width: 318,
                   child: TextField(
                     style: TextStyle(fontSize: 14,fontWeight: FontWeight.w400),
+                      controller: dateInput,
+                      readOnly: true,
+                      onTap: () async {
+                        DateTime? pickedDate = await showDatePicker(
+                            context: context,
+                            initialDate: DateTime.now(),
+                            firstDate: DateTime(1950),
+                            //DateTime.now() - not to allow to choose before today.
+                            lastDate: DateTime(2100));
+
+                        if (pickedDate != null) {
+                          print(
+                              pickedDate); //pickedDate output format => 2021-03-10 00:00:00.000
+                          String formattedDate =
+                          DateFormat('dd/MM/yyyy').format(pickedDate);
+                          print(
+                              formattedDate); //formatted date output using intl package =>  2021-03-16
+                          setState(() {
+                            dateInput.text =
+                                formattedDate; //set output date to TextField value.
+                          });
+                        } else {}
+                      },
                     decoration: InputDecoration(
                         hintText: "Select Your Date of Birth",
                         suffixIcon: Icon(Icons.calendar_month),
                         suffixIconColor: AppColor.primary,
                         border: OutlineInputBorder(
                           borderRadius: BorderRadius.all(Radius.circular(8)),
-                        )),),),
+                        ))
+                  ),
+                ),
                 SizedBox(height: 100,),
 
                 Container(height: 41,width: 317,
